8a28563192fedf8ff3d8984b707abfdc1910ab4d
Kim Lindgren <kim.lindgren@slu.se>
2022-06-21 15:23:54 +0200

    Some adjustments

--- docker/build-stack.sh
+++ docker/build-stack.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
-docker build -t ditchnet ditchnet
+echo "Create network"
 docker network create ditchnet
+
+echo "Create database container"
 docker run -d --name pg_ditchnet --network ditchnet -e POSTGRES_PASSWORD=$1 --restart unless-stopped postgres:14
-sleep 10 #give the db some time to initialize
+
+echo "Build DitchNet image"
+docker build -t ditchnet ditchnet
+
+echo "Initialize database"
 docker cp ../db.sql pg_ditchnet:/db.sql
 docker exec -t pg_ditchnet psql -U postgres -f /db.sql
 docker exec -t pg_ditchnet rm -f /db.sql
+
+echo "Start DitchNet container"
 docker run -d --name ditchnet --network ditchnet -p 5566:5566 --restart unless-stopped ditchnet
